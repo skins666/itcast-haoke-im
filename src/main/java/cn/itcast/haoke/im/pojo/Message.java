151edfbb3ae655e3ce79d719117fc8c6539757e7
rsz <2445400433@qq.com>
2023-06-04 15:40:02 +0800

    Initial commit

---
+++ src/main/java/cn/itcast/haoke/im/pojo/Message.java
@@ -0,0 +1,40 @@
+package cn.itcast.haoke.im.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+//import org.bson.types.ObjectId;
+//import org.springframework.data.annotation.Id;
+//import org.springframework.data.mongodb.core.index.Indexed;
+//import org.springframework.data.mongodb.core.mapping.Document;
+//import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+//@Document(collection = "message") // 指定表的名称
+@Builder
+public class Message {
+
+//    @Id
+    private String id;
+    private String msg;
+    /**
+     * 消息状态，1-未读，2-已读
+     */
+//    @Indexed
+    private Integer status;
+//    @Field("send_date")
+//    @Indexed
+    private Date sendDate;
+//    @Field("read_date")
+    private Date readDate;
+//    @Indexed
+    private User from;
+//    @Indexed
+    private User to;
+
+}
